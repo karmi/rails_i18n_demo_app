6e97e6e165f8b65bd06fc1d6ac3a6a5371df3e4b
Karel Minarik <karmi@karmi.cz>
2008-08-22 11:42:24 +0200

    Added some basic tests of locale as a blueprint for locale testing

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,4 +1,23 @@
+require 'date'
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
+  
+  def test_should_render_proper_en_us_locale_by_default
+    get :index
+    assert_equal "Good bye!", assigns(:good_bye)
+    assert_select 'p.hello_world', :text => 'Hello World!'
+    assert_select 'p.currency',    :text => '$1,024.00'
+    # TODO: Implement mocha and +falsify_today+ method
+    assert_select 'span.localized_date',    :text => Date.today.strftime("%Y-%m-%d")
+  end
+  
+  def test_should_render_proper_text_in_czech_locale
+    get :index, :locale => 'cz'
+    assert_equal "Nashledanou!", assigns(:good_bye)
+    assert_select 'p.hello_world', :text => 'Dobrý den!'
+    assert_select 'p.currency',    :text => '1 024,00 Kč'
+    assert_select 'span.localized_date',    :text => Date.today.strftime("22. 08. 2008")
+  end
+  
 end
