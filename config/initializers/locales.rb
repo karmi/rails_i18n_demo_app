b08609c47241b5a8b0b4b910f084e327bad1f50c
Sven Fuchs <svenfuchs@artweb-design.de>
2008-08-23 15:11:30 +0200

    load translation files through I18n.load_translations in intializer

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,4 +1,15 @@
 # Load locales from RAILS_ROOT/lib/locale directory into Rails
-I18n.backend.populate do
-  Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each { |l| require l }
+# I18n.backend.populate do
+#   Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each { |l| require l }
+# end
+
+# I18n.populate do
+#   Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each do |file| 
+#     I18n.load_translations file
+#   end
+# end
+
+I18n.populate do
+  I18n.load_translations "#{RAILS_ROOT}/lib/locale/cz.rb"
+  I18n.load_translations "#{RAILS_ROOT}/lib/locale/de.yml"
 end
