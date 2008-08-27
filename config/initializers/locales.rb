9410eccf8adc21582de94d3f002081bba53ec007
Sven Fuchs <svenfuchs@artweb-design.de>
2008-08-27 11:35:11 +0200

    #pluralize has been removed from the I18n api

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,19 +1,7 @@
 # Load locales from RAILS_ROOT/lib/locale directory into Rails
-# I18n.backend.populate do
-#   Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each { |l| require l }
-# end
-
-# I18n.populate do
-#   Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each do |file| 
-#     I18n.load_translations file
-#   end
-# end
-
-# require 'i18n/backend/chain'
+# Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each { |l| require l }
 
 # will be populated to the simple backend
-I18n.populate do
-  I18n.load_translations "#{RAILS_ROOT}/lib/locale/cz.rb"
-  I18n.load_translations "#{RAILS_ROOT}/lib/locale/de.yml"
-  I18n.load_translations "#{RAILS_ROOT}/lib/locale/en-US.yml"
-end
+I18n.load_translations "#{RAILS_ROOT}/lib/locale/cz.rb"
+I18n.load_translations "#{RAILS_ROOT}/lib/locale/de.yml"
+I18n.load_translations "#{RAILS_ROOT}/lib/locale/en-US.yml"
