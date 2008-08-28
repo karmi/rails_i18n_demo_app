b7839ac1b2b569b99ee116bcf9313ff7f3b68558
Karel Minarik <karmi@karmi.cz>
2008-08-28 11:14:03 +0200

    Making loading locales from +lib/locales+ dynamic again (Loads any .rb or .yml you drop there)

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,7 +1,3 @@
 # Load locales from RAILS_ROOT/lib/locale directory into Rails
-# Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.rb') ].each { |l| require l }
-
 # will be populated to the simple backend
-I18n.load_translations "#{RAILS_ROOT}/lib/locale/cz.rb"
-I18n.load_translations "#{RAILS_ROOT}/lib/locale/de.yml"
-I18n.load_translations "#{RAILS_ROOT}/lib/locale/en-US.yml"
+Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ].each { |l| I18n.load_translations l }
