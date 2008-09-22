3b2d44cf52a75ae1c7fdf6feb56823a48c47abd4
Karel Minarik <karmi@karmi.cz>
2008-09-22 10:54:06 +0200

    Fixed loading of locales with the new i18n changes. See http://github.com/rails/rails/commit/a3b7fa78bfdc33e45e39c095b67e02d50a2c7bea

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,3 +1,3 @@
 # Load locales from RAILS_ROOT/lib/locale directory into Rails
 # will be populated to the simple backend
-Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ].each { |l| I18n.load_translations l }
+I18n.load_path += Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ]
