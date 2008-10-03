e526afa88e91e6d6551d4652a5a2cef210d53dfe
Karel Minarik <karmi@karmi.cz>
2008-10-03 14:56:08 +0200

    TODO : Use Sven's cleaner suggestion for getting available locales

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -2,5 +2,6 @@
 # Will be populated to the simple backend
 I18n.load_path += Dir[ File.join(RAILS_ROOT, 'lib', 'locale', '*.{rb,yml}') ]
 # Get loaded locale keys into global variable
+# TODO: Use Sven's suggestion from http://rails-i18n.org/wiki/pages/i18n-available_locales
 I18n.backend.send(:init_translations) # Hack? Hell, yeah :(
 AVAILABLE_LOCALES = I18n.backend.send(:instance_variable_get, :@translations).keys.collect { |l| l.to_s }
