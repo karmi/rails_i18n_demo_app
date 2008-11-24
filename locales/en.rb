978eb04b209cc8deb0d68057e5df5ab20c6d2fc6
Karel Minarik <karmi@karmi.cz>
2008-11-24 11:52:04 +0100

    Clarify documentation in "default" English locale file

--- locales/en.rb
+++ locales/en.rb
@@ -1,4 +1,12 @@
 # Default locale for standard Rails internalization
+
+# Default locale in Rails 2.2.2 is <tt>:en</tt>
+
+# All the strings in Rails 2.2.2 are localized, see eg. <tt>activerecord/lib/active_record/locale/en.yml</tt>
+# See http://github.com/rails/rails/tree/2-2-stable/activerecord%2Flib%2Factive_record%2Flocale%2Fen.yml
+
+# You essentially "override" this localization in your own locale files
+
 # See www.rails-i18n.org for more information about Rails i18n 
 
 { :en => {
